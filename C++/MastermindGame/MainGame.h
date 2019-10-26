e51b85a513e6f3239e6a977f013d843fe26f8c7e
LauraCD <56573281+LauraCD@users.noreply.github.com>
2019-10-26 20:57:24 +1100

    Create MainGame.h

---
+++ C++/MastermindGame/MainGame.h
@@ -0,0 +1,60 @@
+#pragma once
+/**************************************************
+Project: MasterMind TG
+Assignment 3
+Author: Laura Chauca Diaz
+Purpose: Driver Header File
+**************************************************/
+#ifndef MAIN_H
+#define MAIN_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+
+#include "Board.h"
+#include "Player.h"
+using namespace std;
+
+// Variables
+///for Board info
+int codeElements;
+int skillLevel;
+int numGuess;
+int totalGuess;
+///for Player info
+int gameWin;
+int score;
+
+///for game loop(s)
+bool isGameOver;
+bool hasGuessedCode;
+
+int lose;
+int win;
+
+string name;
+string playerGuess;
+string currentCode;
+int codeLength;
+string codeDoc;
+vector<string> codeList;
+
+string askForString(string question);
+int baseScore(char level, char elements);
+char askForChar(string question);
+
+// Game functions
+void runMenu();
+void displayTitle();
+void displayText(string myFileName);
+void gameOver();
+void playGame();
+void updateWin();
+void updateLose();
+void createCode(int level, int elements);
+void setCodeLists(string myFileName, int codeSize);
+
+#endif
